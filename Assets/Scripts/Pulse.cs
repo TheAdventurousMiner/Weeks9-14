b04e92378581708b6243dc6a827bbaeae1695a76
TheAdventurousMiner <csantospablos@gmail.com>
2025-08-12 19:53:43 -0400

    completed pulse task for coding gym.

--- Assets/Scripts/Pulse.cs
+++ Assets/Scripts/Pulse.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Pulse : MonoBehaviour
 {
-    private float speed = 3f;
+    private float speed = 10f;
+    public AnimationCurve pulseCurve;
+    private float timePassed;
   
     // Start is called before the first frame update
     void Start()
     {
-        
+        timePassed = 0f;
     }
 
     // Update is called once per frame
@@ -17,14 +20,23 @@ public class Pulse : MonoBehaviour
     {
         transform.position += Vector3.right * speed * Time.deltaTime;
 
+        timePassed += Time.deltaTime;
+
         Vector3 pulsePosition = Camera.main.WorldToScreenPoint(transform.position);
 
         if (pulsePosition.x > Screen.width)
         {
             pulsePosition.x = 0;
+            timePassed = 0f;
         }
 
-        transform.position = Camera.main.ScreenToWorldPoint(pulsePosition);
+        float yCurve = pulseCurve.Evaluate(timePassed);
+
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(pulsePosition);
+
+        transform.position = new Vector3(newPosition.x , yCurve, 0f);
+
+       
 
     }
 }
